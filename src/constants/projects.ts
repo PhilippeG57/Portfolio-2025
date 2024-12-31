7586768c0d07756af31cafeaeade14f779508233
Philippe Garcia <philippe.garcia@alsace.eu>
2024-12-31 10:10:15 +0100

    FIX projects

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -1,3 +1,4 @@
+import chateauxRhenans from '../media/projets/chateauxrhenans.jpg';
 import vendezFacile from '../media/projets/vendezfacile.png';
 import waysWater from '../media/projets/wwt.png';
 import amongusImg from '../media/projets/amongus.jpg';
@@ -7,12 +8,18 @@ import azcomm from '../media/projets/azcomm.jpg';
 import cocovoit from '../media/projets/cocovoit.jpg';
 import demay from '../media/projets/demay.jpg';
 import fmcbd from '../media/projets/fmcbd.png';
-import lettreMotiv from '../media/projets/lettreMotiv.png';
-import questionChampion from '../media/projets/questionchampion.jpg';
 import sophiemare from '../media/projets/sophiemare.jpg';
 import viedemome from '../media/projets/viedemome.png';
 
 export const projectsList = [
+  {
+    href: "https://www.chateaux-rhenans.eu/",
+    title: 'Chateaux Rhénans',
+    description: 'Un site web crée avec ASP.NET et le CMS Umbraco',
+    image: chateauxRhenans,
+    tags: ['ASP.NET', 'umbraco'],
+    category: 'professionnel',
+  },
   {
     href: "https://vendezfacile.com/",
     title: 'Vendez Facile',
@@ -80,7 +87,7 @@ export const projectsList = [
   {
     href: "https://arobase-design.fr/shop57455/",
     title: 'Arobase Shop',
-    description: 'Un site web crée avec le CMS PrestaShop.',
+    description: 'Un site ecommerce crée avec le CMS PrestaShop.',
     image: arobaseshop,
     tags: ['PHP', 'Prestashop', 'Orienté Objet'],
     category: 'professionnel',
@@ -93,24 +100,6 @@ export const projectsList = [
     tags: ['HTML', 'CSS', 'Javascript', 'PHP'],
     category: 'professionnel',
   },
-  {
-    href: "https://philippe-garcia.fr/lettreMotivation/",
-    title: 'Générateur de lettre de motivation',
-    description: 'Une application créée avec les langages natifs HTML, CSS et PHP',
-    image: lettreMotiv,
-    tags: ['HTML', 'CSS', 'PHP'],
-    category: 'personnel',
-  },
-  {
-    href: "https://philippe-garcia.fr/questionchampion/",
-    title: 'Générateur de questions',
-    description: 'Une application créée avec les langages natifs HTML, CSS, Javascript et PHP',
-    image: questionChampion,
-    tags: ['HTML', 'CSS', 'Javascript', 'PHP'],
-    category: 'personnel',
-  },
-
-
   {
     href: "https://philippe-garcia.fr/amongus/",
     title: 'Among Us',
